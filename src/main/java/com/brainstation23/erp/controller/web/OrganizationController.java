1fc4850ba98838f4702826c278cc69eeb199dff1
jamilxt <jamilxt@gmail.com>
2023-04-11 18:06:12 +0600

    Add OrganizationController for web

---
+++ src/main/java/com/brainstation23/erp/controller/web/OrganizationController.java
@@ -0,0 +1,10 @@
+package com.brainstation23.erp.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "organizations")
+public class OrganizationController {
+	// TODO: write Organizations CRUD with template engine (thymeleaf)
+}
