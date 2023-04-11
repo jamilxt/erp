5e16a7f3ef902e2ab8613d3108d7224cc575450d
jamilxt <jamilxt@gmail.com>
2023-04-11 17:08:15 +0600

    Initial commit

---
+++ src/test/java/com/brainstation23/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainstation23.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
