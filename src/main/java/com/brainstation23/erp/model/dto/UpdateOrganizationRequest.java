55df0bc4fa2f98d5c8df3bcb8f24635c7bba4a7e
jamilxt <jamilxt@gmail.com>
2023-04-11 18:05:08 +0600

    Add validation for CreateOrganizationRequest & UpdateOrganizationRequest

--- src/main/java/com/brainstation23/erp/model/dto/UpdateOrganizationRequest.java
+++ src/main/java/com/brainstation23/erp/model/dto/UpdateOrganizationRequest.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
+
 @ToString
 @Getter
 @Setter
 public class UpdateOrganizationRequest {
+	@NotNull
 	private String name;
 }
